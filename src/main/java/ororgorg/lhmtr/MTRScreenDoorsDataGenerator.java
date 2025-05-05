3031e64782d78d138bb2c7bce0b420dbea2c9768
YL Yang <143940926+DarkMasterXPGD@users.noreply.github.com>
2025-05-05 11:36:36 +0800

    Upload

---
+++ src/main/java/ororgorg/lhmtr/MTRScreenDoorsDataGenerator.java
@@ -0,0 +1,11 @@
+package ororgorg.lhmtr;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MTRScreenDoorsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
