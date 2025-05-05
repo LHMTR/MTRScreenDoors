4f626d933f73908bafaa1773e05a3967454486e6
YL Yang <143940926+DarkMasterXPGD@users.noreply.github.com>
2025-05-05 11:15:49 +0800

    。

---
+++ src/main/kotlin/org/lhmtr/MTRScreenDoorsDataGenerator.kt
@@ -0,0 +1,9 @@
+package org.lhmtr
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MTRScreenDoorsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
