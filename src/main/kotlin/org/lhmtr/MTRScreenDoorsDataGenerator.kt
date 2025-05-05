f5630252aef42db45517ee15474e6b0c38a4a74e
YL Yang <143940926+DarkMasterXPGD@users.noreply.github.com>
2025-05-05 11:33:45 +0800

    Delete

--- src/main/kotlin/org/lhmtr/MTRScreenDoorsDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package org.lhmtr
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object MTRScreenDoorsDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
